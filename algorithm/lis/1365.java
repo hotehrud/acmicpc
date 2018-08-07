bcf551add197a94b35b9cd15f6caaac5c0af54af
mygumi <hotehrud@naver.com>
2018-08-07 11:39:04 +0900

    재채점, LIS n^2 => nlogn 개선

--- algorithm/lis/1365.java
+++ algorithm/lis/1365.java
@@ -1,99 +1,98 @@
-package craw;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+  // http://mygumi.tistory.com/303
+  private void solve() {
+    int n = sc.nextInt();
+    int[] array = new int[n];
+    int[] lis = new int[n];
 
-	private void solve() {
-		
-		// LIS
-		
-		int n = sc.nextInt();
-		int[] array = new int[n+1];
-		int[] dp = new int[n+1];
-		int max = 0;
-		
-		for(int i=0;i<n;i++) {
-			array[i] = sc.nextInt();
-		}
-		
-		for(int i=0;i<n;i++) {
-			
-			dp[i] = 1;
-			
-			for(int j=0;j<i;j++) {
-				
-				if (array[i] > array[j] && dp[j] + 1 > dp[i]) {
-					dp[i] = dp[j] + 1;
-				}
-				
-			}
-			
-			if (max < dp[i]) {
-				max = dp[i];
-			}
-			
-		}
-		
-		System.out.println(n - max);
-		
-	}
-	
-	public static void main(String[] args) {
-		sc.init();
+    for (int i = 0; i < n; i++) {
+      array[i] = sc.nextInt();
+    }
 
-		new Main().solve();
-	}
+    int idx = 0;
+    lis[0] = array[0];
+    for (int i = 1; i < n; i++) {
+      if (lis[idx] < array[i]) {
+        lis[++idx] = array[i];
+      } else {
+        lis[lower_bound(lis, idx, array[i])] = array[i];
+      }
+    }
+    System.out.println(n - (idx + 1));
+  }
 
-	static class sc {
-		private static BufferedReader br;
-		private static StringTokenizer st;
+  public static int lower_bound(int[] array, int end, int n) {
+    int start = 0;
 
-		static void init() {
-			br = new BufferedReader(new InputStreamReader(System.in));
-			st = new StringTokenizer("");
-		}
+    while (start < end) {
+      int mid = (end + start) / 2;
 
-		static String readLine() {
-			try {
-				return br.readLine();
-			} catch (IOException e) {
-			}
-			return null;
-		}
+      if (array[mid] < n) {
+        start = mid + 1;
+      } else {
+        end = mid;
+      }
+    }
 
-		static String readLineReplace() {
-			try {
-				return br.readLine().replaceAll("\\s+", "");
-			} catch (IOException e) {
-			}
-			return null;
-		}
+    return end;
+  }
 
-		static String next() {
-			while (!st.hasMoreTokens()) {
-				try {
-					st = new StringTokenizer(br.readLine());
-				} catch (IOException e) {
-				}
-			}
-			return st.nextToken();
-		}
+  public static void main(String[] args) {
+    sc.init();
 
-		static long nextLong() {
-			return Long.parseLong(next());
-		}
+    new Main().solve();
+  }
 
-		static int nextInt() {
-			return Integer.parseInt(next());
-		}
+  static class sc {
+    private static BufferedReader br;
+    private static StringTokenizer st;
 
-		static double nextDouble() {
-			return Double.parseDouble(next());
-		}
-	}
+    static void init() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+      st = new StringTokenizer("");
+    }
+
+    static String readLine() {
+      try {
+        return br.readLine();
+      } catch (IOException e) {
+      }
+      return null;
+    }
+
+    static String readLineReplace() {
+      try {
+        return br.readLine().replaceAll("\\s+", "");
+      } catch (IOException e) {
+      }
+      return null;
+    }
+
+    static String next() {
+      while (!st.hasMoreTokens()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+        }
+      }
+      return st.nextToken();
+    }
+
+    static long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    static int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    static double nextDouble() {
+      return Double.parseDouble(next());
+    }
+  }
 }

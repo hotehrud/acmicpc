125b2434d15234a06d5d05417ec84bf6f00372cd
mygumi <hotehrud@naver.com>
2019-06-19 14:21:19 +0900

    K번째 수 && Collection.sort

---
+++ algorithm/sort/11004.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+  private void solve() {
+    int n = sc.nextInt();
+    int k = sc.nextInt();
+    ArrayList<Integer> array = new ArrayList<Integer>();
+
+    for (int i = 0; i < n; i++) {
+      array.add(sc.nextInt());
+    }
+
+    Collections.sort(array);
+    System.out.println(array.get(k - 1));
+  }
+
+  public static void main(String[] args) {
+    sc.init();
+
+    new Main().solve();
+  }
+
+  static class sc {
+    private static BufferedReader br;
+    private static StringTokenizer st;
+
+    static void init() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+      st = new StringTokenizer("");
+    }
+
+    static String readLine() {
+      try {
+        return br.readLine();
+      } catch (IOException e) {
+      }
+      return null;
+    }
+
+    static String readLineReplace() {
+      try {
+        return br.readLine().replaceAll("\\s+", "");
+      } catch (IOException e) {
+      }
+      return null;
+    }
+
+    static String next() {
+      while (!st.hasMoreTokens()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+        }
+      }
+      return st.nextToken();
+    }
+
+    static boolean hasNext() {
+      return st.hasMoreTokens();
+    }
+
+    static long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    static int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    static double nextDouble() {
+      return Double.parseDouble(next());
+    }
+  }
+}

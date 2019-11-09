04b1a987695da0f4ab64f076fda37a6e9ae2f282
hotehrud <hotehrud@naver.com>
2019-11-09 17:51:06 +0900

    정올 초등, 고등 && 컬러볼

--- KOI/2015/10800.java
+++ KOI/2015/10800.java
@@ -3,65 +3,69 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
-	// https://github.com/hotehrud/acmicpc
-	// http://mygumi.tistory.com/273
 	private void solve() {
+		// https://mygumi.tistory.com/273
 		int n = sc.nextInt();
 		ArrayList<Ball> input = new ArrayList<Ball>();
-		ArrayList<Integer>[] colors = (ArrayList<Integer>[]) new ArrayList[200001];
-		int[] sizes = new int[2001];
-		int[] psum = new int[200001];
+		int[] players = new int[200001];
+		int[] colors = new int[200001];
 		StringBuilder sb = new StringBuilder();
 
-		for (int i = 0; i < 200001; i++) {
-			colors[i] = new ArrayList<Integer>();
-		}
-
-		for (int i = 1; i <= n; i++) {
+		for (int i = 0; i < n; i++) {
 			int color = sc.nextInt();
 			int size = sc.nextInt();
-			input.add(new Ball(color, size));
-			colors[color].add(size);
-			sizes[size]++;
+			input.add(new Ball(i, color, size));
 		}
 
-		// 부분합
-		for (int i = 1; i < 2001; i++) {
-			psum[i] = psum[i - 1] + (sizes[i] * i);
-		}
+		Collections.sort(input, new Comparator<Ball>() {
+			public int compare(Ball b1, Ball b2) {
+				if (b1.size < b2.size) {
+					return -1;
+				} else if (b1.size > b2.size) {
+					return 1;
+				} else {
+					if (b1.color < b2.color) {
+						return -1;
+					} else {
+						return 1;
+					}
+				}
+			}
+		});
 
-		for (int i = 1; i <= n; i++) {
-			Collections.sort(colors[i]);
+		int sum = 0;
+		for (int i = 0, j = 0; i < n; i++) {
+			Ball a = input.get(i);
+			Ball b = input.get(j);
+
+			while (b.size < a.size) {
+				sum += b.size;
+				colors[b.color] += b.size;
+
+				b = input.get(++j);
+			}
+			players[a.id] = sum - colors[a.color];
 		}
 
 		for (int i = 0; i < n; i++) {
-			Ball b = input.get(i);
-			int c = b.c;
-			int s = b.s;
-			int sum = psum[s - 1];
-
-			for (int same : colors[c]) {
-				if (same < s) {
-					sum -= same;
-				} else {
-					break;
-				}
-			}
-			sb.append(sum + "\n");
+			sb.append(players[i] + "\n");
 		}
 		System.out.println(sb.toString());
 	}
 
 	public static class Ball {
-		int c;
-		int s;
+		int id;
+		int color;
+		int size;
 
-		Ball(int c, int s) {
-			this.c = c;
-			this.s = s;
+		Ball(int id, int color, int size) {
+			this.id = id;
+			this.color = color;
+			this.size = size;
 		}
 	}
 

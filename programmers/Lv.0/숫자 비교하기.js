db912c4370a82c2efc1e49898d13b1e77fb87cfe
mygumi <hotehrud@naver.com>
2023-04-29 13:50:21 +0900

    feat: Lv.0 120807

---
+++ programmers/Lv.0/숫자 비교하기.js
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120807
+// 본인 풀이
+function solution(num1, num2) {
+  return num1 === num2 ? 1 : -1;
+}
+
+
+// 다른 사람의 풀이
+function solution(num1, num2) {
+  return ((num1 == num2)-0.5)*2;
+}

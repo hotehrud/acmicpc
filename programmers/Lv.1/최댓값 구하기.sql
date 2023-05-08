59083701aebd20e3c7d77da7882b1988d9398ead
mygumi <hotehrud@naver.com>
2023-05-08 21:34:47 +0900

    feat: Lv.1 59415

---
+++ programmers/Lv.1/최댓값 구하기.sql
@@ -0,0 +1,14 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59415
+-- 본인 풀이
+SELECT
+    MAX(DATETIME)
+FROM ANIMAL_INS
+
+-- 다른 사람의 풀이
+SELECT datetime as 시간
+from animal_ins
+where datetime = (select max(datetime) from animal_ins);
+
+SELECT datetime as 시간
+from animal_ins
+order by datetime desc limit 1
